29c91f2ffeae37059c9869672f4b575c9dcaa463
shital@zestard.net <shital@zestard.net>
2020-10-05 23:54:11 +0530

    completed login authantication

--- instagram-clone/src/App.js
+++ instagram-clone/src/App.js
@@ -4,7 +4,7 @@ import Modal from "@material-ui/core/Modal";
 import "./App.css";
 import instanamelogo from "./images/Instagram_logo.png";
 import Post from "./Components/Post";
-import { db,auth } from "./firebase";
+import { db, auth } from "./firebase";
 import { Button, Input } from "@material-ui/core";
 
 function getModalStyle() {
@@ -34,30 +34,31 @@ function App() {
 
   const [posts, setPosts] = useState([]);
   const [open, setOpen] = useState(false);
+  const [openSignIn,setOpenSignIn] = useState(false);
   const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [user,setUser] = useState(null);
+  const [user, setUser] = useState(null);
 
   const [modalStyle] = React.useState(getModalStyle);
 
-  useEffect(()=>{
-    const unsubscribe = auth.onAuthStateChanged((authUser)=>{
-      if(authUser){
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((authUser) => {
+      if (authUser) {
         // user has logged in...
+        console.log(authUser);
         setUser(authUser);
-      }
-      else{
+      } else {
         // user has logged out...
         setUser(null);
       }
-    })
+    });
 
-    return () =>{
+    return () => {
       //perform clean up
       unsubscribe();
-    }
-  },[user,username])
+    };
+  }, [user, username]);
 
   // useEffect -> Runs a piece of code on a specific conditions
   useEffect(() => {
@@ -73,15 +74,25 @@ function App() {
 
   const handleSignup = (e) => {
     e.preventDefault();
-    auth.createUserWithEmailAndPassword(email,password)
-    .then((authUser)=>{
-      authUser.user.updateProfile({
-        displayName:username
+    auth
+      .createUserWithEmailAndPassword(email, password)
+      .then((authUser) => {
+        authUser.user.updateProfile({
+          displayName: username,
+        });
       })
-    })
-    .catch((error)=>{alert(error.message)})
+      .catch((error) => {
+        alert(error.message);
+      });
   };
 
+  const signIn = (event)=>{
+    event.preventDefault();
+    auth.signInWithEmailAndPassword(email,password)
+    .catch((error)=>alert(error.message))
+
+    setOpenSignIn(false);
+  }
   return (
     <div className="App">
       <Modal open={open} onClose={() => setOpen(false)}>
@@ -116,17 +127,45 @@ function App() {
         </div>
       </Modal>
 
+      <Modal open={openSignIn} onClose={() => setOpenSignIn(false)}>
+        <div style={modalStyle} className={classes.paper}>
+          <form className="app_signup">
+            <center>
+              <div>
+                <img className="app_headerImage" src={instanamelogo} alt="" />
+              </div>
+            </center>
+
+            <Input
+              type="text"
+              placeholder="Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <Input
+              type="text"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <Button onClick={signIn}>Sign In</Button>
+          </form>
+        </div>
+      </Modal>
+
       <div className="app_header">
         <img className="app_headerImage" src={instanamelogo} alt="" />
       </div>
 
-      <Button
-        onClick={() => {
-          setOpen(true);
-        }}
-      >
-        Sign Up
-      </Button>
+      {user ? (
+        <Button onClick={() => auth.signOut()}> Logout </Button>
+      ) : (
+        <div className='app_loginContainer'>
+          <Button onClick={() => { setOpenSignIn(true);}}> Sign In </Button>
+          <Button onClick={() => { setOpen(true);}}> Sign Up </Button>
+         </div> 
+      )}
+
       {posts.map(({ id, post }) => {
         return (
           <Post
